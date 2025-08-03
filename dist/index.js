c02b8387fc6f8645738a635f476fe29e38c5964b
Najlae Abarghache <najlae.abarghache1@gmail.com>
2025-08-03 01:41:18 +0100

    Readme file

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.ts
-export * from "./types/mini-game-types";
-export * from "./types/decision-tree";
-export * from "./utils/order-miniGames";
-export * from "./utils/config-loader";
-import miniGames from "./config/mini-games.json";
-import defaultTestConfig from "./config/default-test-config.json";
-export { miniGames, defaultTestConfig };
